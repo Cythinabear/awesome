a76e7ea922b1e315a52605a9ee386de250bf4b4f
Cythinabear <qing1114299560@qq.com>
2016-09-06 23:06:15 +0800

    updata url

--- www/handlers.py
+++ www/handlers.py
@@ -10,8 +10,21 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
         '__template__': 'blogs.html',
-        'users': users
+        'blogs': blogs
     }
+
+# API: 获取用户信息
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
